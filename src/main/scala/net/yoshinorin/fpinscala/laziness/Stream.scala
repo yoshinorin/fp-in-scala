695472f8edd4fc605c4f7937f437cabb53c7f3d3
yoshinorin <yoshinorin.net@outlook.com>
2022-02-23 21:24:18 +0900

    update

--- src/main/scala/net/yoshinorin/fpinscala/laziness/Stream.scala
+++ src/main/scala/net/yoshinorin/fpinscala/laziness/Stream.scala
@@ -1,10 +1,54 @@
 package net.yoshinorin.fpinscala.laziness
 
-trait Stream[+A]
+import net.yoshinorin.fpinscala.laziness.Stream.empty
+
+import scala.annotation.tailrec
+
+trait Stream[+A] {
+
+  def toList: List[A] = {
+    @tailrec
+    def run(s: Stream[A], accm: List[A]): List[A] = s match {
+      case Cons(h, t) => run(t(), h() :: accm)
+      case _ => accm
+    }
+    run(this, List()).reverse
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n == 1 => Stream.cons(h(), empty)
+    case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n - 1))
+    case _ => empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+  def takeWhile(f: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if f(h()) => Stream.cons(h(), t().takeWhile(f))
+    case _ => empty
+  }
+
+  def exists(f: A => Boolean): Boolean = this match {
+    // f(h()) がtrueを返した場合に以降は評価されない
+    case Cons(h, t) => f(h()) || t().exists(f)
+    case _ => false
+  }
+
+  // f の第二パラメータは非正格
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+}
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
+  // スマートコンストラクタ
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
@@ -17,6 +61,7 @@ object Stream {
     if (as.isEmpty) {
       empty
     } else {
+      // 引数はサンクにまとめられる。評価されるまで評価されない
       cons(as.head, apply(as.tail: _*))
     }
   }
---
+++ src/test/scala/net/yoshinorin/fpinscala/laziness/StreamSpec.scala
@@ -0,0 +1,37 @@
+package net.yoshinorin.fpinscala.laziness
+
+import net.yoshinorin.fpinscala.laziness.Stream._
+import org.scalatest.wordspec.AnyWordSpec
+
+// testOnly net.yoshinorin.fpinscala.laziness.StreamSpec
+class StreamSpec extends AnyWordSpec {
+
+  "Option" should {
+
+    "toList" in {
+      println("\n-------- toList")
+      val s = Stream("a", "b", "c")
+      println(s.toList)
+    }
+
+    "take" in {
+      println("\n-------- take")
+      val s = Stream("a", "b", "c", "d", "e")
+      println(s.take(2).toList)
+    }
+
+    "drop" in {
+      println("\n-------- drop")
+      val s = Stream("a", "b", "c", "d", "e")
+      println(s.drop(3).toList)
+    }
+
+    "takeWhile" in {
+      println("\n-------- takeWhile")
+      val s = Stream("a", "b", "c", "d", "e")
+      println(s.takeWhile(x => x != "c").toList)
+    }
+
+  }
+
+}
