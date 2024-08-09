b3ce50c8f74db8ca4e045e9e339f837b1549065c
yoshinorin <yoshinorin.net@outlook.com>
2024-08-09 20:03:36 +0900

    update

--- src/main/scala/example/ch2/GettingStarted.scala
+++ src/main/scala/example/ch2/GettingStarted.scala
@@ -14,7 +14,73 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit = println(formatAbs(-42))
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  // 単相関数
+  private def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if(n >= ss.length) {
+        -1
+      } else if (ss(n) == key) {
+        n
+      } else {
+        loop(n + 1)
+      }
+    }
+
+    loop(0)
+  }
+
+  // 多相関数（総称関数）
+  private def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if(n >= as.length) {
+        -1
+      } else if (as(n) == p) {
+        n
+      } else {
+        loop(n + 1)
+      }
+    }
+
+    loop(0)
+  }
+
+
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean = {
+      if (n >= as.length - 1) {
+        true
+      } else if (gt(as(n), as(n + 1))) {
+        false
+      } else {
+        go(n + 1)
+      }
+    }
+
+    go(0)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    println(formatAbs(-42))
+    println(formatFactorial(7))
+
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+  }
+
 
   def factorial(n: Int): Int = {
     def go(n: Int, acc: Int): Int = {
@@ -24,6 +90,18 @@ object MyModule {
         go(n - 1, n * acc)
       }
     }
+    go(n, 1)
+  }
+
+  def fib(n: Int): Int = {
+    def go(n: Int, current: Int, next: Int): Int = {
+      if (n <= 0) {
+        current
+      } else {
+        go(n - 1, next, current + next)
+      }
+    }
+    go(0, 0, 1)
   }
 
 }
