3918e4f5887ff58fde52c5bb1f8ecb81e03fa532
yoshinorin <yoshinorin.net@outlook.com>
2024-08-30 19:18:56 +0900

    update

--- src/main/scala/example/ch2/GettingStarted.scala
+++ src/main/scala/example/ch2/GettingStarted.scala
@@ -104,4 +104,22 @@ object MyModule {
     go(0, 0, 1)
   }
 
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
+    // (b: B) => f(a, b)
+    // コンテキストから `b` の型を把握しているので `(b: B)` と記述する必要はない
+    b => f(a, b)
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    a => b => f(a, b)
+  }
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
+
 }
